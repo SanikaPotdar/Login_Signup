7d2dd65cf3db0e8f3beda1a8b61e709f199f1ab2
AmeyaDaddikar <ameyadaddikar@gmail.com>
2017-10-27 18:09:51 +0530

    updated UI

---
+++ userdemo/src/userdemo/Userdemo.java
@@ -0,0 +1,14 @@
+
+package userdemo;
+
+public class Userdemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Signup().setVisible(true);
+        
+    }
+    
+}
